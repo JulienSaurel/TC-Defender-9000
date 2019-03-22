5cab7eb6bc8d42d86eeef21b7bbd115887f58064
Erlkaen <tilopapin@gmail.com>
2019-03-22 14:43:49 +0100

    intervals

--- JS/script.js
+++ JS/script.js
@@ -36,11 +36,6 @@ var reloadTime = MAX_RELOAD_TIME; //temps de rechargement (en ms) du tir
 var bullet_damages = 1;
 var bullets = Array(); //tableau contenant toutes les bullet tirées
 
-//explosions
-var explosionWidth = 100;
-var explosionHeight = 100;
-var explosions = Array();
-
 //ennemies
 var ENNEMY_WIDTH = (1 / 12) * 0.5 * axeX; //largeur d'un ennemi
 var ENNEMY_HEIGHT = (1 / 12) * 0.55 * axeY; //hauteur d'un ennemi
@@ -66,7 +61,6 @@ var tcTwoBSprite = new Image(); //sprite du TC lvl 2 tourné dans l'autre sens
 var tcThreeASprite = new Image(); //sprite du TC lvl 3 tourné dans un sens
 var tcThreeBSprite = new Image(); //sprite du TC lvl 3 tourné dans l'autre sens
 var microWaveSprite = new Image();
-var explosionSprite = new Image();
 
 //position de la souris
 var angle; //angle (en rad) entre le centre du canon et la position de la souris (recalculé en permanence)
@@ -94,7 +88,8 @@ var BULLET_SPEED_MAX = 5;
 var infiniteWaveTimer = 5000;
 var spawnables = Array();
 var wn = 1;
-var spawnInterval;
+var intervals = Array();;
+
 //permet de charger les images en leur attribuant une source
 function loadImages(){
   bulletSprite.src = "../img/belette.png";
@@ -109,7 +104,6 @@ function loadImages(){
   tcThreeASprite.src = "../img/TC3_A.png";
   tcThreeBSprite.src = "../img/TC3_B.png";
   microWaveSprite.src = "../img/micro_onde.png";
-  explosionSprite.src = "../img/explosion.png";
 }
 
 //fonction appelée au lancement
@@ -130,10 +124,14 @@ function initialise(){
   setTimeout(function(){
     document.addEventListener("click", canon.shoot, false); //listener pour le click de souris
   }, 100);
-  clearInterval(spawnInterval);
-  spawnInterval = undefined;
+
   clearInterval(interval);
   interval = undefined;
+
+  intervals.forEach(function(interv){
+    clearInterval(interv);
+    interv = undefined;
+  });
   spawnables = new Array();
   ennemies = new Array();
 }
@@ -291,73 +289,6 @@ canon = {
   },
 }
 
-canonLeft = {
-  x: 50, // coordonnée X du canon
-  y: 50, // coordonnée Y du canon
-  width: CANON_WIDTH, // largeur du canon
-  height: CANON_HEIGHT, // hauteur du canon
-  angle: 0, //orientation du canon
-  canShoot: true, //variable vérifiant si le canon est en rechargement
-  lvl: 1 ,
-  sprite: canonASprite,
-  // fonction qui dessine le canon
-  draw: function(){
-    //on dessine le canon
-    contextCanon.drawImage(canon.sprite, this.x, this.y, this.width, this.height);
-
-    //on réinitialiser l'orientation du canvas du canon
-    contextCanon.setTransform(1, 0, 0, 1, 0, 0);
-
-    //on déplace le canvas aux coordonnée du centre du canon
-    contextCanon.translate((CANON_X + CANON_WIDTH / 2), (CANON_Y + CANON_HEIGHT / 2));
-
-    //on effectue une rotation selon l'angle
-    contextCanon.rotate(canon.angle);
-
-    // on effectue la translation inverse
-    contextCanon.translate(-(CANON_X + CANON_WIDTH / 2), -(CANON_Y + CANON_HEIGHT / 2));
-  },
-
-  //fonction permettant d'actualiser les données du canon
-  update: function(){
-    if(canon.lvl === 1){
-      canon.sprite = canonASprite;
-    }else if(canon.lvl === 2){
-      canon.sprite = canonBSprite;
-    }else if(canon.lvl === 3){
-      canon.sprite = canonCSprite;
-    }else{
-      canon.sprite = canonDSprite;
-    }
-  },
-
-  //fonction permettant au canon de tirer
-  shoot: function(e){
-    //on vérifie que le canon puisse tirer
-    if(canon.canShoot === true){
-      //on dit que le canon ne peut plus tirer
-      canon.canShoot = false;
-
-      //on enregistre les coordonnée du click de souris
-      let mouseClickX = e.clientX - window.innerWidth * 0.125;
-      let mouseClickY = e.clientY;
-
-      //on créer un point en fonction de ces coordonnées
-      pointDest = {x: mouseClickX, y: mouseClickY};
-
-      //on instancie une nouvelle bullet à l'aide de ce point
-      bullets.push(new Bullet(pointDest));
-
-
-      //on dit qu'après le temps de rechargement, le canon peut de nouveau tirer
-      setTimeout(function(){
-        canon.canShoot = true;
-      }, reloadTime);
-    }
-  },
-}
-
-
 microWave = {
   x: microWave_X,
   y: microWave_Y,
@@ -441,11 +372,9 @@ function objectToPoint(object, point) {
   }
 }
 
-//fonction qui donne une direction de déplacement à un objet
-function objectToDirection(object, point){
-  //cette fonction fonctionne comme "objectToPoint" sauf qu'elle ne donne que les vitesses X et Y à l'objet
-  let AC = point.y - object.y;
-  let CB = point.x - object.x;
+function shootBullet(object, point){
+  let AC = point.y - (canon.x + canon.width / 2);
+  let CB = point.x - (canon.y + canon.height / 2);
   let AB = Math.sqrt(AC * AC + CB * CB);
   let ratio = Math.floor(AB / object.speed);
   let diffX = CB / ratio;
@@ -454,17 +383,6 @@ function objectToDirection(object, point){
   let y = point.y;
   object.speedX = diffX;
   object.speedY = diffY;
-}
-
-function bulletToDirection(bullet){
-  let canonX = axeX / 2;
-  let canonY = axeY / 2;
-  let endX = canonPoint().x;
-  let endY = canonPoint().y;
-
-  let AB = endX - canonX;
-  let BC = end
-
 }
 
 //fonction calculant les coordonnées du bout du canon en fonction de son angle
@@ -507,25 +425,6 @@ function Bullet(pointDest){
         this.y += this.speedY;
     }
   }
-}
-
-function Explode(point, width, height){
-  this.x = point.x + width / 2;
-  this.y = point.y + height / 2;
-  this.with = explosionWidth;
-  this.height = explosionHeight;
-  this.sprite = explosionSprite;
-  this.active = true;
-
-  this.draw = function(){
-    if(this.active === true){
-      context.drawImage(this.sprite, this.x, this.y, this.width, this.height);
-    }
-  };
-
-  this.update = function(){
-
-  };
 }
 
 //constructeur d'un ennemi
@@ -646,9 +545,6 @@ function bulletCollider(){
                 playHitSound();
               }
 
-              //explosions
-              explosions.push(new Explode({x: bullet.x, y: bullet.y,}, bullet.width, bullet.height));
-
               //la bullet se désactive
               bullet.active = false;
 
@@ -717,7 +613,7 @@ function playUnlimitedWave(){
 }
 
 function playWave(wn, timeInterval){
-  spawnInterval = setInterval(function(){
+   intervals.push(setInterval(function(){
     if(paused === false){
       ennemies.push(spawnables[0]);
       spawnables.shift();
@@ -725,7 +621,7 @@ function playWave(wn, timeInterval){
         spawnables = Array();
       }
     }
-  }, timeInterval);
+  }, timeInterval));
 }
 
 function playWaveNumber(wn){
@@ -739,7 +635,7 @@ function playWaveNumber(wn){
     }else if(wn === 3){
       waveThree(50);
     }else if(wn === 4){
-      waveFour(200);
+      waveFour(100);
     }
     playWave(wn, 1500);
   }
@@ -747,7 +643,7 @@ function playWaveNumber(wn){
 
 function playGame(){
   playWaveNumber(wn);
-  let newInterval = setInterval(function(){
+    intervals.push(setInterval(function(){
     if(spawnables.length === 0){
       let nextWave = true;
       for (var i = 0; i < ennemies.length; i++) {
@@ -771,7 +667,7 @@ function playGame(){
         nextWave = false;
       }
     }
-  }, 10);
+  }, 10));
 }
 
 function garbageCollector(){
@@ -789,12 +685,8 @@ function garbageCollector(){
 
   bullets = bullets.filter(bullet => bullet != undefined);
 
-  for (var i = 0; i < explosions.length; i++) {
-    if(explosions[i] != undefined && explosions[i].active === false)
-    delete explosions[i];
-  }
+  intervals = intervals.filter(interv => interv != undefined);
 
-  explosions = explosions.filter(explosion => explosion != undefined);
 }
 
 function actualiseHP(){
@@ -993,10 +885,6 @@ function update(){
         ennemy.update();
       }
     });
-
-    explosions.forEach(function(explosion){
-      explosion.update();
-    });
   }
 }
 
@@ -1009,7 +897,7 @@ function draw(){
     bullets.forEach(function(bullet){
       if(bullet != undefined){
         if(!bullet.launched){
-          objectToDirection(bullet, bullet.pointDest);
+          shootBullet(bullet, bullet.pointDest);
           bullet.launched = true;
         }
         bullet.draw();
@@ -1026,11 +914,6 @@ function draw(){
         }
         ennemy.draw();
       }
-    });
-
-
-    explosions.forEach(function(explosion){
-      explosion.draw();
     });
 
     canon.draw();
