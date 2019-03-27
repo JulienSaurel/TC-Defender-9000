0d54c82e2484d1b96aaa4e12e1d10b80acae8fc9
DaRKSTeVuS <darkstevus@gmail.com>
2019-03-27 22:10:30 +0100

    ajout de sécurité minime

--- php/controller/routeur.php
+++ php/controller/routeur.php
@@ -5,6 +5,8 @@ if (isset($_GET['action'])) {
     if (in_array($_GET['action'], get_class_methods('ControllerScore'))) {
         $action = $_GET['action'];
         ControllerScore::$action();
+    } else {
+        die("Erreur : petit coquin");
     }
 } else {
     die("Erreur : accés non autorisé !");
